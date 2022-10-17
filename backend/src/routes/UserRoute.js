da00dbdc9ef49d9ae0178b30a6da0eeeeff86852
kunal.zanke <kunal.zanke@globallogic.com>
2022-10-17 19:25:42 +0530

    empty repo

--- backend/src/routes/UserRoute.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const authController = require('../controllers/authController')
-
-const userRoutes = Router();
-
-userRoutes.post("/authenticate" , authController.authenticateUser)
-userRoutes.post("/addUser" , authController.addUser)
-
-module.exports = userRoutes;
